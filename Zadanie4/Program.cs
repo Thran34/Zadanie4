6c38c588ebc3ec3a4d5429d8913ebb487d3999a5
Andrzej Jasiński <thran34@interia.pl>
2022-07-11 12:46:12 +0200

    Add project files.

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Zadanie4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = (string.Join(" ", args));
+            if (number.Contains("."))
+            {
+                number = number.Replace(".", "");
+            }
+            else if (number.Contains(","))
+            {
+                number = number.Replace(",", "");
+            }
+            Console.WriteLine(number.Length);
+        }
+    }
+}
